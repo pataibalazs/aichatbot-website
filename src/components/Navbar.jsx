f817480b63b74cf2d6c89a1c8ea5b2cf877ce70b
pataibalazs <79509500+pataibalazs@users.noreply.github.com>
2023-09-12 23:40:55 +0200

    hero good design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,34 +16,34 @@ export function Navbar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <header className="bg-white">
-    <nav className="mx-auto flex max-w-7xl items-center p-6 lg:px-8 z-400000000" aria-label="Global">
-      <div className="flex items-center">
-        <a href="#" className="-m-1.5 p-1.5">
-          <span className="sr-only">Your Company</span>
-          <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
-        </a>
-      </div>
-      <div className="flex-grow lg:flex lg:items-center lg:justify-center">
-        <div className="hidden lg:flex lg:gap-x-12">
-          {navigation.map((item) => (
-            <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
-              {item.name}
-            </a>
-          ))}
+    <header className="bg-pageColors-2">
+      <nav className="mx-auto flex max-w-7xl items-center p-6 lg:px-8 z-400000000" aria-label="Global">
+        <div className="flex items-center">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">Your Company</span>
+            <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+          </a>
         </div>
-      </div>
-      <div className="flex items-center">
-        <button
-          type="button"
-          className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700 lg:hidden"
-          onClick={() => setMobileMenuOpen(true)}
-        >
-          <span className="sr-only">Open main menu</span>
-          <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-        </button>
-      </div>
-    </nav>
+        <div className="flex-grow lg:flex lg:items-center lg:justify-center">
+          <div className="hidden lg:flex lg:gap-x-12">
+            {navigation.map((item) => (
+              <a key={item.name} href={item.href} className="text-base font-semibold leading-6 text-pageColors-1">
+                {item.name}
+              </a>
+            ))}
+          </div>
+        </div>
+        <div className="flex items-center">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700 lg:hidden"
+            onClick={() => setMobileMenuOpen(true)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+      </nav>
       <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className="" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
@@ -66,7 +66,7 @@ export function Navbar() {
             </button>
           </div>
           <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
+            <div className="">
               <div className="space-y-2 py-6">
                 {navigation.map((item) => (
                   <a
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pageColors: {
+          1: '#160e18',
+          2: '#f7d568',
+          3: '#f7c15b',
+          4: '#ffffff',
+          5: '#f2a74c',
+        },
+      }
+    },
   },
   plugins: [require('@tailwindcss/forms')],
 }
